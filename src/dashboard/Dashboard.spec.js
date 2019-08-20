645fa3935923eea13450b666f1feaf9b9a5854c3
Jay Wood <47437698+jaywood8@users.noreply.github.com>
2019-08-20 12:52:40 -0700

    Added integration testing, Dashboard testing complete

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import  { render, fireEvent } from "@testing-library/react";
+import  { render, cleanup, fireEvent } from "@testing-library/react/pure";
 import Dashboard from "./Dashboard";
+import { id } from 'postcss-selector-parser';
 
 
-describe("<Dashboard />", () => {
-      
-    it("matches snapshot", () => {
-        const tree = renderer.create(<Dashboard />);
-        expect(tree.toJSON()).toMatchSnapshot(); 
-    });
+describe("<Dashboard />", () => {   
+    beforeEach(cleanup);
+    it("renders without crashing", () => {
+        render(<Dashboard />);
+    });   
+
+});
+
+describe("<Dashboard /> state transitions", () => {
+
+    const { getByText } = render(<Dashboard  />);
+
+    it("default state is open and unlocked", () => {
+        //verify open and unlocked, using regular expressions ^, $
+        getByText(/^open$/i);
+        getByText(/^unlocked$/i);
+        getByText(/^lock gate$/i);
+        getByText(/^close gate$/i);
+    })
     
+    it("open and unlocked to closed and unlocked", () => {
+        const closeBtn = getByText(/^close gate$/i);
+        fireEvent.click(closeBtn);
+    //check display updated after click event:
+        getByText(/^closed$/i);
+        getByText(/^unlocked$/i);
+
+        getByText(/^lock gate$/i);
+        getByText(/^open gate$/i);
+    })
+
 });
 
 
